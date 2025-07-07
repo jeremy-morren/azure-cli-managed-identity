ce1717101d7a5b03634ea0ee9758d9091e833bd7
Jeremy Morren <jeremy.morren@outlook.com>
2025-07-07 18:15:22 +0800

    Working on win container

---
+++ src/Container/InstallAzureCliTrimmed.ps1
@@ -0,0 +1,28 @@
+﻿param (
+    [string]$ZipFile,
+    [string]$OutputPath
+)
+
+function GetDirectorySize {
+    param (
+        [string]$Path
+    )
+    $size = 0
+    Get-ChildItem -Recurse -File -Path $Path | ForEach-Object { $size += $_.Length }
+    return $size
+}
+
+Expand-Archive -Path $ZipFile -DestinationPath $OutputPath -Force
+
+# We will only use az get-access-token
+# Remove large unnecessary files to installation size
+
+$sitePackages = Join-Path $OutputPath 'Lib\site-packages'
+
+$largeSize = 500 * 1000 # 500 KB
+Get-ChildItem -Directory (Join-Path $sitePackages "azure/cli/command_modules") `
+    | ? { (GetDirectorySize -Path $_.FullName) -gt $largeSize } `
+    | Remove-Item -Recurse -Force
+
+Get-ChildItem -Directory (Join-Path $sitePackages "azure/mgmt/resource/policy") | Remove-Item -Recurse -Force
+Get-ChildItem -Directory (Join-Path $sitePackages "azure/mgmt/resource/resources") | Remove-Item -Recurse -Force
